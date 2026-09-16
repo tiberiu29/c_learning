fe4c5c2cd98173e46e1284aba331d1aafd5190a6
Tibi <tibi@Florentinas-MacBook-Pro.local>
2026-09-16 20:33:18 +0300

    feat: Added unity test framework

---
+++ dynamic-array/test/test_arraylist.c
@@ -0,0 +1,20 @@
+#include "unity.h"
+#include "arraylist.h"
+
+void setUp(void) {
+
+}
+
+void tearDown() {
+
+}
+
+void test_initialize_empty_array_list(void) {
+
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_initialize_empty_array_list);
+    return UNITY_END();
+}
